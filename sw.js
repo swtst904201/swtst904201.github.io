aa35b93c3d8eadbb522326cae8efdf694305ca00
Alan Su <alsu@google.com>
2019-04-30 10:46:50 -0700

    debug log msg

--- sw.js
+++ sw.js
@@ -9,5 +9,6 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   console.log('fetch: ' + event)
   console.log('event.request.url: ' + event.request.url)
+  console.log('redirect!')
   event.respondWith(Response.Response().redirect('https://glgtxhw4ouy7nhsepgqbtysvjn7ivoasvfzu37mn2ga6do2ktzda.litepages.googlezip.net/favicon.ico', 302))
 });

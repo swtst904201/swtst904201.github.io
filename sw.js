8eadbd0b1d434a7d24f1599143701b2d83aaf18b
Alan Su <alsu@google.com>
2019-04-30 12:23:30 -0700

    debug logging

--- sw.js
+++ sw.js
@@ -8,8 +8,9 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('fetch: ' + event)
-  console.log('event.request.url: ' + event.request.url)
+  console.log('event.request.url: [' + event.request.url + ']')
   if (/^.*\/jpg$/.test(event.request.url)) {
+    console.log('matched')
     event.respondWith(Response.redirect('https://glgtxhw4ouy7nhsepgqbtysvjn7ivoasvfzu37mn2ga6do2ktzda.litepages.googlezip.net/favicon.ico', 302))
   }
 });

33f274a0d3ff9935b091206a3fc3f64735a77331
Alan Su <alsu@google.com>
2019-04-30 12:20:29 -0700

    only intercept the jpg request

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   console.log('fetch: ' + event)
   console.log('event.request.url: ' + event.request.url)
-  if (/^.*\/favicon.ico$/.test(event.request.url)) {
+  if (/^.*\/jpg$/.test(event.request.url)) {
     event.respondWith(Response.redirect('https://glgtxhw4ouy7nhsepgqbtysvjn7ivoasvfzu37mn2ga6do2ktzda.litepages.googlezip.net/favicon.ico', 302))
   }
 });

35e9620e1b0aef43c6e3ef76790563289e9b6ce1
Alan Su <alsu@google.com>
2019-04-30 09:49:08 -0700

    add activate event handler

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ self.addEventListener('install', event => {
   console.log('install: ' + event)
 });
 
+self.addEventListener('activate', event => {
+  console.log('activate: ' + event)
+});
+
 self.addEventListener('fetch', event => {
   console.log('event: ' + event)
 });

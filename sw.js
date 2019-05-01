8b321e2658d922d9034f73075e42c58555ebe6cb
Alan Su <alsu@google.com>
2019-05-01 12:02:39 -0700

    cleanup

--- sw.js
+++ sw.js
@@ -1,19 +1,11 @@
-self.addEventListener('install', event => {
-  console.log('install: ' + event)
-});
-
-self.addEventListener('activate', event => {
-  console.log('activate: ' + event)
-});
-
 const encodeStd = 'abcdefghijklmnopqrstuvwxyz234567'
 function lpHost(origin) {
   const mbuf = new TextEncoder('utf-8').encode(origin)
-  var s = ""
-  const hashPromise = crypto.subtle.digest('SHA-256', mbuf).then(function(hbuf) {
+  return crypto.subtle.digest('SHA-256', mbuf).then(function(hbuf) {
     const harr = Array.from(new Uint8Array(hbuf));
     const hhex = harr.map(b => ('00' + b.toString(16)).slice(-2)).join('');
 
+    var s = ""
     var b = 0;
     var bits = 0;
     for (var i = 0; i < harr.length; i++) {
@@ -31,9 +23,7 @@ function lpHost(origin) {
       b = b << (5-bits);
       s += encodeStd[b];
     }
-  });
 
-  return hashPromise.then(function() {
     return s + ".litepages.googlezip.net";
   });
 }
@@ -42,9 +32,7 @@ function lpURL(origURL) {
   const url = new URL(origURL);
   const origin = "https://" + url.hostname + ":443";
   return lpHost(origin).then(function(h) {
-    const urlStr = "https://" + h + "/sr?u=" + encodeURI(origURL);
-    console.log('urlStr=' + urlStr);
-    return new URL(urlStr);
+    return newURL("https://" + h + "/sr?u=" + encodeURI(origURL));
   });
 }
 
@@ -57,19 +45,11 @@ const lpAllowedDestinations = [
 ];
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
-  console.log('fetch: ' + event);
-  console.log('event.request.url: [' + event.request.url + ']');
-  console.log('event.request.destination: ' + event.request.destination);
-  // if (/^.*\.jpg$/.test(event.request.url)) {
   if (lpAllowedHosts.includes(url.hostname) &&
       lpAllowedDestinations.includes(event.request.destination)) {
-    console.log('matched: [https://' + url.hostname + ':443]')
     event.respondWith(lpURL(event.request.url).then(function(ru) {
-      console.log('ru=' + ru);
+      console.log('redirect [' + event.request.url + '] => [' + ru + ']');
       return Response.redirect(ru, 307);
     }));
-    // lpURL(event.request.url).then(function(ru) {
-    //   console.log(event.request.url + ' => ' + ru)
-    // });
   }
 });
